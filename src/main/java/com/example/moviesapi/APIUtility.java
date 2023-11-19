f37aa679369f4e25367163bc581302fa7b020905
Hetashvi Ban <hetashvi7988@gmail.com>
2023-11-19 15:20:35 -0500

    Added the ability to call the ODMb APi

---
+++ src/main/java/com/example/moviesapi/APIUtility.java
@@ -0,0 +1,30 @@
+package com.example.moviesapi;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class APIUtility {
+    /*This method will call the ODMb API from the with the movie title passed as an argument*/
+    public static void callAPI(String movieName) throws IOException, InterruptedException {
+        //If we received "Star Wars", we need to translate that to be "Star%20Wars"
+        movieName = movieName.replace(" ", "%20"); /*This will replace all the space with %20*/
+
+        String uri = "https://www.omdbapi.com/?apikey=f54d92f0&s="+movieName;
+
+        //configure the environment to make a HTTP request
+        //(this includes an update to the module-info.java file)
+        HttpClient client = HttpClient.newHttpClient();
+        //The below requests the API code
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(uri)).build();
+
+        //this will save a file called movies.json file with the API's response
+        //This will response to the http request
+        HttpResponse<Path> httpResponse = client.send(httpRequest, HttpResponse.BodyHandlers
+                .ofFile(Paths.get("movies.json")));
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.moviesapi {
     requires javafx.controls;
     requires javafx.fxml;
-            
+    requires java.net.http;
                             
     opens com.example.moviesapi to javafx.fxml;
     exports com.example.moviesapi;
